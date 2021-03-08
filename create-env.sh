5069f911c8f2f6e0d7c8bc23e6d192383eddc4fb
SAPMANPC\Mosacho <sapirush97@gmail.com>
2021-03-08 17:05:31 +0200

    Fixed create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -1,4 +1,4 @@
 echo "MESSAGE=${MESSAGE}" > .env
-echo "ACCESS_KEY=${ACCESS_KEY}" > .env
-echo "SECRET_KEY=${SECRET_KEY}" > .env
-echo "BUCKET_NAME=${BUCKET_NAME}" > .env
+echo "ACCESS_KEY=${ACCESS_KEY}" >> .env
+echo "SECRET_KEY=${SECRET_KEY}" >> .env
+echo "BUCKET_NAME=${BUCKET_NAME}" >> .env
