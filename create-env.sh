1069e3b0a5d32369cb08c58ab089fc8cd71b48e7
SAPMANPC\Mosacho <sapirush97@gmail.com>
2021-05-29 20:56:17 +0300

    Added sessionToken to create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -1,4 +1,5 @@
 echo "MESSAGE=${MESSAGE}" > .env
 echo "ACCESS_KEY=${ACCESS_KEY}" >> .env
 echo "SECRET_KEY=${SECRET_KEY}" >> .env
+echo "SESSION_TOKEN=${SESSION_TOKEN}" >> .env
 echo "BUCKET_NAME=${BUCKET_NAME}" >> .env
